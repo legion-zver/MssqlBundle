fe09a1652c342ae6dffd2c0718b16a791a497093
Александр <garin1221@yandex.ru>
2017-02-15 18:00:26 +0500

    - Fixes

--- Platforms/DblibPlatform.php
+++ Platforms/DblibPlatform.php
@@ -24,6 +24,10 @@ namespace Realestate\MssqlBundle\Platforms;
 use Doctrine\DBAL\DBALException,
     Doctrine\DBAL\Schema\TableDiff;
 
+use Doctrine\DBAL\Schema\Column;
+use Doctrine\DBAL\Schema\ColumnDef;
+use Doctrine\DBAL\Schema\Identifier;
+
 use Doctrine\DBAL\Platforms\AbstractPlatform;
 use Doctrine\DBAL\Platforms\SQLServer2008Platform;
 
@@ -191,7 +195,228 @@ class DblibPlatform extends SQLServer2008Platform
             $typeDecl = $field['type']->getSqlDeclaration($field, $this);
             $columnDef = $typeDecl . $collation . $default . $notnull . $unique . $check;
         }
-
         return $name . ' ' . $columnDef;
+    }
+    /**
+     * {@inheritDoc}
+     */
+    public function getAlterTableSQL(TableDiff $diff)
+    {
+        $queryParts  = array();
+        $sql         = array();
+        $columnSql   = array();
+        $commentsSql = array();
+
+        /** @var \Doctrine\DBAL\Schema\Column $column */
+        foreach ($diff->addedColumns as $column) {
+            if ($this->onSchemaAlterTableAddColumn($column, $diff, $columnSql)) {
+                continue;
+            }
+
+            $columnDef = $column->toArray();
+            $queryParts[] = 'ADD ' . $this->getColumnDeclarationSQL($column->getQuotedName($this), $columnDef);
+
+            //TODO: Сделать поддержку конфигурирования
+            //if (isset($columnDef['default'])) {
+            //$queryParts[] = $this->getAlterTableAddDefaultConstraintClause($diff->name, $column);
+            //}
+
+            $comment = $this->getColumnComment($column);
+
+            if ( ! empty($comment) || is_numeric($comment)) {
+                $commentsSql[] = $this->getCreateColumnCommentSQL(
+                    $diff->name,
+                    $column->getQuotedName($this),
+                    $comment
+                );
+            }
+        }
+
+        foreach ($diff->removedColumns as $column) {
+            if ($this->onSchemaAlterTableRemoveColumn($column, $diff, $columnSql)) {
+                continue;
+            }
+            /** TODO: Внедрить:
+            SELECT
+            default_constraints.name
+            FROM
+            sys.all_columns
+
+            INNER JOIN
+            sys.tables
+            ON all_columns.object_id = tables.object_id
+
+            INNER JOIN
+            sys.schemas
+            ON tables.schema_id = schemas.schema_id
+
+            INNER JOIN
+            sys.default_constraints
+            ON all_columns.default_object_id = default_constraints.object_id
+
+            WHERE
+            schemas.name = 'dbo'
+            AND tables.name = 'tablename'
+            AND all_columns.name = 'columnname'
+             */
+
+            $queryParts[] = 'DROP COLUMN ' . $column->getQuotedName($this);
+        }
+
+        /* @var $columnDiff \Doctrine\DBAL\Schema\ColumnDiff */
+        foreach ($diff->changedColumns as $columnDiff) {
+            if ($this->onSchemaAlterTableChangeColumn($columnDiff, $diff, $columnSql)) {
+                continue;
+            }
+
+            $column     = $columnDiff->column;
+            $comment    = $this->getColumnComment($column);
+            $hasComment = ! empty ($comment) || is_numeric($comment);
+
+            if ($columnDiff->fromColumn instanceof Column) {
+                $fromComment    = $this->getColumnComment($columnDiff->fromColumn);
+                $hasFromComment = ! empty ($fromComment) || is_numeric($fromComment);
+
+                if ($hasFromComment && $hasComment && $fromComment != $comment) {
+                    $commentsSql[] = $this->getAlterColumnCommentSQL(
+                        $diff->name,
+                        $column->getQuotedName($this),
+                        $comment
+                    );
+                } elseif ($hasFromComment && ! $hasComment) {
+                    $commentsSql[] = $this->getDropColumnCommentSQL($diff->name, $column->getQuotedName($this));
+                } elseif ($hasComment) {
+                    $commentsSql[] = $this->getCreateColumnCommentSQL(
+                        $diff->name,
+                        $column->getQuotedName($this),
+                        $comment
+                    );
+                }
+            } else {
+                // todo: Original comment cannot be determined. What to do? Add, update, drop or skip?
+            }
+
+            // Do not add query part if only comment has changed.
+            if ($columnDiff->hasChanged('comment') && count($columnDiff->changedProperties) === 1) {
+                continue;
+            }
+
+            $requireDropDefaultConstraint = $this->alterColumnRequiresDropDefaultConstraint($columnDiff);
+
+            if ($requireDropDefaultConstraint) {
+                $queryParts[] = $this->getAlterTableDropDefaultConstraintClause(
+                    $diff->name,
+                    $columnDiff->oldColumnName
+                );
+            }
+
+            $columnDef = $column->toArray();
+
+            $queryParts[] = 'ALTER COLUMN ' .
+                $this->getColumnDeclarationSQL($column->getQuotedName($this), $columnDef);
+
+            if (isset($columnDef['default']) && ($requireDropDefaultConstraint || $columnDiff->hasChanged('default'))) {
+                $queryParts[] = $this->getAlterTableAddDefaultConstraintClause($diff->name, $column);
+            }
+        }
+
+        foreach ($diff->renamedColumns as $oldColumnName => $column) {
+            if ($this->onSchemaAlterTableRenameColumn($oldColumnName, $column, $diff, $columnSql)) {
+                continue;
+            }
+
+            $oldColumnName = new Identifier($oldColumnName);
+
+            $sql[] = "sp_RENAME '" .
+                $diff->getName($this)->getQuotedName($this) . "." . $oldColumnName->getQuotedName($this) .
+                "', '" . $column->getQuotedName($this) . "', 'COLUMN'";
+
+            // Recreate default constraint with new column name if necessary (for future reference).
+            if ($column->getDefault() !== null) {
+                $queryParts[] = $this->getAlterTableDropDefaultConstraintClause(
+                    $diff->name,
+                    $oldColumnName->getQuotedName($this)
+                );
+                $queryParts[] = $this->getAlterTableAddDefaultConstraintClause($diff->name, $column);
+            }
+        }
+
+        $tableSql = array();
+
+        if ($this->onSchemaAlterTable($diff, $tableSql)) {
+            return array_merge($tableSql, $columnSql);
+        }
+
+        foreach ($queryParts as $query) {
+            $sql[] = 'ALTER TABLE ' . $diff->getName($this)->getQuotedName($this) . ' ' . $query;
+        }
+
+        $sql = array_merge($sql, $commentsSql);
+
+        if ($diff->newName !== false) {
+            $sql[] = "sp_RENAME '" . $diff->getName($this)->getQuotedName($this) . "', '" . $diff->getNewName()->getName() . "'";
+
+            /**
+             * Rename table's default constraints names
+             * to match the new table name.
+             * This is necessary to ensure that the default
+             * constraints can be referenced in future table
+             * alterations as the table name is encoded in
+             * default constraints' names.
+             */
+            $sql[] = "DECLARE @sql NVARCHAR(MAX) = N''; " .
+                "SELECT @sql += N'EXEC sp_rename N''' + dc.name + ''', N''' " .
+                "+ REPLACE(dc.name, '" . $this->generateIdentifierName($diff->name) . "', " .
+                "'" . $this->generateIdentifierName($diff->newName) . "') + ''', ''OBJECT'';' " .
+                "FROM sys.default_constraints dc " .
+                "JOIN sys.tables tbl ON dc.parent_object_id = tbl.object_id " .
+                "WHERE tbl.name = '" . $diff->getNewName()->getName() . "';" .
+                "EXEC sp_executesql @sql";
+        }
+
+        $sql = array_merge(
+            $this->getPreAlterTableIndexForeignKeySQL($diff),
+            $sql,
+            $this->getPostAlterTableIndexForeignKeySQL($diff)
+        );
+
+        return array_merge($sql, $tableSql, $columnSql);
+    }
+
+    /** COPY FROM SQLServerPlatform **/
+
+    private function getAlterTableAddDefaultConstraintClause($tableName, Column $column) {
+        $columnDef = $column->toArray();
+        $columnDef['name'] = $column->getQuotedName($this);
+        return 'ADD' . $this->getDefaultConstraintDeclarationSQL($tableName, $columnDef);
+    }
+
+    private function alterColumnRequiresDropDefaultConstraint(ColumnDef $columnDiff) {
+        if ( ! $columnDiff->fromColumn instanceof Column) {
+            return false;
+        }
+        if ($columnDiff->fromColumn->getDefault() === null) {
+            return false;
+        }
+        if ($columnDiff->hasChanged('default')) {
+            return true;
+        }
+        if ($columnDiff->hasChanged('type') || $columnDiff->hasChanged('fixed')) {
+            return true;
+        }
+        return false;
+    }
+
+    private function generateIdentifierName($identifier) {
+        $identifier = new Identifier($identifier);
+        return strtoupper(dechex(crc32($identifier->getName())));
+    }
+
+    private function generateDefaultConstraintName($table, $column) {
+        return 'DF_' . $this->generateIdentifierName($table) . '_' . $this->generateIdentifierName($column);
+    }
+
+    private function getAlterTableDropDefaultConstraintClause($tableName, $columnName) {
+        return 'DROP CONSTRAINT ' . $this->generateDefaultConstraintName($tableName, $columnName);
     }
 }
